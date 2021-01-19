db476425ec3c1e10fe6e391fb22bb28e106595fc
taylor-young0 <59883690+taylor-young0@users.noreply.github.com>
2021-01-19 10:50:33 -0500

    Increased the number of 'Hello World' prints to 15

--- lab01/HelloWorld.java
+++ lab01/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < 15; i++) {
       System.out.println("Hello world!");
     }
   }
