0854e95a5f93b43018772dc4c470d4513e8c276d
taylor-young0 <59883690+taylor-young0@users.noreply.github.com>
2021-01-19 10:49:07 -0500

    Added counter beside each 'Hello World' print

--- lab01/HelloWorld.java
+++ lab01/HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
   public static void main(String[] args) {
     for (int i = 0; i < 5; i++) {
-      System.out.println("Hello world!");
+      System.out.println("Hello world! " + i);
     }
   }
 }
